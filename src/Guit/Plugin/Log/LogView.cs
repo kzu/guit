25df84280aaadac2b3d8f898c9dad687941a0f80
Adrian Alonso <adrianalonso@gmail.com>
2019-09-13 10:30:24 -0300

    Listing a basic list of commits in the Log view

--- src/Guit/Plugin/Log/LogView.cs
+++ src/Guit/Plugin/Log/LogView.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Composition;
+using LibGit2Sharp;
 using Terminal.Gui;
 
 namespace Guit.Plugin.Log
@@ -9,16 +11,68 @@ namespace Guit.Plugin.Log
     [Export]
     public class LogView : ContentView
     {
+        readonly Repository repository;
+
+        List<CommitInfo> commits = new List<CommitInfo>();
+        readonly ListView view;
+
         [ImportingConstructor]
-        public LogView()
+        public LogView(Repository repository)
             : base("Log")
         {
-            Content = new View
+            this.repository = repository;
+
+            view = new ListView(commits)
             {
-                new Label("// TODO: Log")
+                AllowsMarking = true
             };
+
+            Content = view;
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+
+            commits = repository.Commits
+                .QueryBy(new CommitFilter())
+                .Select(x =>
+                    new CommitInfo
+                    {
+                        Message = x.MessageShort,
+                        Author = x.Author.Name,
+                        When = x.Committer.When
+                    })
+                .Take(50)
+                .ToList();
+
+            view.SetSource(commits);
         }
 
         public override string Context => nameof(Log);
+
+        class CommitInfo
+        {
+            public string Message { get; set; }
+
+            public string Author { get; set; }
+
+            public DateTimeOffset When { get; set; }
+
+            public override string ToString()
+            {
+                return GetNormalizedString(Message, 70) +
+                    GetNormalizedString(Author, 25) +
+                    When;
+            }
+
+            string GetNormalizedString(string value, int length)
+            {
+                if (value.Length < length)
+                    return string.Concat(value, new String(' ', length - value.Length));
+
+                return value;
+            }
+        }
     }
 }
