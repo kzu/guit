66f86cb08a26af19e256b7be0b28409bbec300ba
Adrian Alonso <adrianalonso@gmail.com>
2019-09-23 20:05:23 -0300

    Added Revert file changes command

---
+++ src/Guit/Dialogs/MessageBox.cs
@@ -0,0 +1,28 @@
+﻿using Terminal.Gui;
+
+namespace Guit
+{
+    /// <summary>
+    /// Message box dialog for showing messages.
+    /// </summary>
+    class MessageBox : DialogBox
+    {
+        public MessageBox(string title, string message)
+            : base(title)
+        {
+            Message = message;
+        }
+
+        protected override void EndInit()
+        {
+            Width = 60;
+            Height = 10;
+
+            Add(new Label(string.Empty) { Y = 1, X = 1, Width = Dim.Fill(2) }, nameof(Message));
+
+            base.EndInit();
+        }
+
+        public string Message { get; set; }
+    }
+}
