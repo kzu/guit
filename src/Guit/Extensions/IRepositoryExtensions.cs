a474b2ecdaa68f65ba45221f00805e0e1b9ae594
Adrian Alonso <adrianalonso@gmail.com>
2019-09-23 20:05:23 -0300

    Added Revert file changes command

---
+++ src/Guit/Extensions/IRepositoryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LibGit2Sharp
+{
+    static class IRepositoryExtensions
+    {
+        public static void RevertFileChanges(this IRepository repository, params string[] filePaths) =>
+            repository.CheckoutPaths(
+                repository.Head.FriendlyName,
+                filePaths,
+                new CheckoutOptions { CheckoutModifiers = CheckoutModifiers.Force });
+    }
+}
---
+++ src/Guit/Plugin/Changes/RevertCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using LibGit2Sharp;
+using Merq;
+using Terminal.Gui;
+using Git = LibGit2Sharp.Commands;
+
+namespace Guit.Plugin.Changes
+{
+    [Shared]
+    [MenuCommand("Revert", Key.F7, nameof(Changes))]
+    public class RevertCommand : IMenuCommand
+    {
+        readonly MainThread mainThread;
+        readonly Repository repository;
+        readonly ChangesView changes;
+
+        [ImportingConstructor]
+        public RevertCommand(MainThread mainThread, Repository repository, ChangesView changes)
+        {
+            this.mainThread = mainThread;
+            this.repository = repository;
+            this.changes = changes;
+        }
+
+        public Task ExecuteAsync(CancellationToken cancellation)
+        {
+            var entries = changes.GetMarkedEntries().ToList();
+
+            if (entries.Any())
+            {
+                var dialog = new MessageBox("Revert Changes", string.Format("Are you sure you want to revert {0} item(s)?", entries.Count));
+                var dialogResult = mainThread.Invoke(() => dialog.ShowDialog());
+
+                if (dialogResult == true)
+                {
+                    foreach (var entry in entries)
+                    {
+                        switch (entry.State)
+                        {
+                            case FileStatus.ModifiedInWorkdir: repository.RevertFileChanges(entry.FilePath); break;
+                            case FileStatus.NewInWorkdir: Git.Remove(repository, entry.FilePath); break;
+                        }
+                    }
+
+                    changes.Refresh();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
