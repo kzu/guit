77f5d4f403cf9bd45b64f586be161373b05f1dc3
Adrian Alonso <adrianalonso@gmail.com>
2019-10-01 11:07:46 -0300

    Moved MenuCommand into Guid.Core

--- src/Guit.Core/MenuCommand.cs
+++ src/Guit.Core/MenuCommand.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace Guit
 {
-    class MenuCommand : IMenuCommand
+    public class MenuCommand : IMenuCommand
     {
         readonly Action action;
         readonly Func<Task> function;
