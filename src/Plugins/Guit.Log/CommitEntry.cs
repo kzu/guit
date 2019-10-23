a71275433caeda5264258af3415233648aa3f1f8
Adrian Alonso <adrianalonso@gmail.com>
2019-10-22 21:15:21 -0300

    Added missing file

---
+++ src/Plugins/Guit.Log/CommitEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using LibGit2Sharp;
+
+namespace Guit.Plugin.Log
+{
+    class CommitEntry : IViewPattern
+    {
+        public CommitEntry(IRepository repository, Commit commit)
+        {
+            Repository = repository;
+            Commit = commit;
+        }
+
+        public IRepository Repository { get; }
+
+        public Commit Commit { get; }
+
+        public override int GetHashCode() => Commit.GetHashCode();
+
+        public override bool Equals(object obj) => Commit.Equals((obj as CommitEntry)?.Commit);
+
+        public void View() => Repository.OpenUrl(Commit);
+    }
+}
